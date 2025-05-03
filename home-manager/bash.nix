f5ddf5f9b612956a6c3aaf9cc5003341d649992f
Feuscel <feuscel@feuscel.dev>
2025-05-03 18:27:38 +0200

    add aliases

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -5,7 +5,8 @@ programs.bash = {
 enable = true;
 enableCompletion = true;
 shellAliases = {
-nrs = "sudo nixos-rebuild switch --flask .#nixos-btw"
+nrs = "sudo nixos-rebuild switch --flake .#nixos-btw";
+hms = "home-manager switch --flake .#feuscel@nixos-btw";
 };
 
 };
