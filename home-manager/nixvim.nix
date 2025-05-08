373c2bc54db2b0638718b6004a729b6e3a736408
Feuscel <feuscel@feuscel.dev>
2025-05-08 20:47:11 +0200

    some nvim config

--- home-manager/nixvim.nix
+++ home-manager/nixvim.nix
@@ -8,11 +8,30 @@
 {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
-   ];
+ ];
+  home.shellAliases.v = "nvim";
+
   programs.nixvim = {
     enable = true;
+    defaultEditor = true;
+
+    performance = {
+      combinePlugins = {
+        enable = true;
+        standalonePlugins = [
+          "hmts.nvim"
+          "neorg"
+          "nvim-treesitter"
+        ];
+      };
+      byteCompileLua.enable = true;
+    };
+
+    viAlias = true;
+    vimAlias = true;
 
     colorschemes.catppuccin.enable = true;
     plugins.lualine.enable = true;
+    luaLoader.enable = true;
   };
 }
